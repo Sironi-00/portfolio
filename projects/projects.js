daf500cf2f0bed42e4feb159ebd11c2a56d104e1
Sironi-00 <00sironi@gmail.com>
2023-06-15 12:08:05 +0100

    add img + description for split view

--- projects/projects.js
+++ projects/projects.js
@@ -13,26 +13,27 @@ const createProject = (projectJson, index) => {
   projectNode.className = "project";
   projectNode.innerHTML = `
     <h2>${name}</h2>
-    <div class="preview">
-        <img id="${imageId}" src="${imgSrc[currentImg]}" alt="${name} preview" />
-        <button id="${switchId}-left" class="switch left ${imgSrc.length>1 || 'hidden'}">Prev</button>
-        <button id="${switchId}-right" class="switch right ${imgSrc.length>1 || 'hidden'}">Next</button>
-    </div>
-    <div class="description">
-    <p>
-        ${description.head}
-    </p>
-    <div class="buttons">
-        <a target="_blank" class="${liveLink.length>1 || 'hidden'}" href="${liveLink}">Live</a>
-        <a target="_blank" href="${repoLink}">GitHub</a>
-    </div>
-    <ul class="tools">
-    ${description.tools.map((tool, index) => `<li key="tools${index}">${tool}</li>`).join("")}
-    </ul>
-    <ul>
-      ${description.features.map((line, index) => `<li key="ls${index}">${line}</li>`).join("")}
-    </ul>
-    </div>
+    <div class="moon">
+      <div class="preview">
+          <img id="${imageId}" src="${imgSrc[currentImg]}" alt="${name} preview" />
+          <button id="${switchId}-left" class="switch left ${imgSrc.length>1 || 'hidden'}">Prev</button>
+          <button id="${switchId}-right" class="switch right ${imgSrc.length>1 || 'hidden'}">Next</button>
+      </div>
+      <div class="description">
+          <p>
+              ${description.head}
+          </p>
+          <div class="buttons">
+              <a target="_blank" class="${liveLink.length>1 || 'hidden'}" href="${liveLink}">Live</a>
+              <a target="_blank" href="${repoLink}">GitHub</a>
+          </div>
+          <ul class="tools">
+              ${description.tools.map((tool, index) => `<li key="tools-${index}">${tool}</li>`).join("")}
+          </ul>
+          <ul>
+              ${description.features.map((line, index) => `<li key="ls-${index}">${line}</li>`).join("")}
+          </ul>
+      </div>
     </div>`;
     const changeImg = (isIncrease) => {
       if (isIncrease) {
